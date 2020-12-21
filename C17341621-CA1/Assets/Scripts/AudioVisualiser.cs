5c46f8ce542f305216620ad8a9d4492723067138
c17341621 <c17341621@mydit.ie>
2020-12-21 17:44:13 +0000

    Working Audio Visual Script

---
+++ C17341621-CA1/Assets/Scripts/AudioVisualiser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class AudioVisualiser : MonoBehaviour
+{
+    public AudioSource _audioSource;
+    public static float[] _samples = new float[512];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _audioSource.GetSpectrumData(_samples,0,FFTWindow.Blackman);
+    }
+}
