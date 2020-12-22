57cab32bcd2b73c5d4840061173ae50ddcf91942
c17341621 <c17341621@mydit.ie>
2020-12-22 23:52:55 +0000

    Colour changes based on single hex value

--- C17341621-CA1/Assets/Scripts/InstantiateCubes.cs
+++ C17341621-CA1/Assets/Scripts/InstantiateCubes.cs
@@ -7,13 +7,17 @@ public class InstantiateCubes : MonoBehaviour
     public GameObject _sampleCubePrefab;
     public GameObject _cylinder;
 
-    public float rotateSpeed = 10.0f;
+    public float rotateSpeed = 30.0f;
     GameObject[] _sampleCube = new GameObject[512];
     public float _maxScale = 1500;
     // Start is called before the first frame update
 
-    int sampleSize = 250;
+    int sampleSize = 72;
     int numPillars = 360;
+
+    int hexColour = 0x000001;
+    Color colour;
+
     void Start()
     {
         Vector3 tempVector;
@@ -45,7 +49,9 @@ public class InstantiateCubes : MonoBehaviour
         float r;
         float g;
         float b;
+
         
+
         this.transform.RotateAround(Vector3.zero,Vector3.up,rotateSpeed * Time.deltaTime);
         
         Vector3 tempVector;
@@ -58,27 +64,51 @@ public class InstantiateCubes : MonoBehaviour
                 tempVector = _sampleCube[i].transform.localScale;
                 tempVector.z = (AudioVisualiser._samples[i % sampleSize] * _maxScale + 2);
                _sampleCube[i].transform.localScale = tempVector;
-                r = ((colourCounter * 2f) % 255) / 100f; //((float)System.Math.Pow(colourCounter, 1.1) % (float)255) / 100;
-                g = ((colourCounter * 3f) % 255) / 100f;//((float)System.Math.Pow(colourCounter, 1.2) % (float)255) / 100;
-                b = ((colourCounter * 4f) % 255) / 100f;//((float)System.Math.Pow(colourCounter, 1.3) % (float)255) / 100;
-               _sampleCube[i].transform.GetChild(0).gameObject.GetComponent<Renderer>().material.SetColor("_Color",new Color(r,g,b)); //SetColor("_Color",new Color(r,g,b));
+                //r = ((colourCounter * 2f) % 255) / 100f; //((float)System.Math.Pow(colourCounter, 1.1) % (float)255) / 100;
+                //g = ((colourCounter * 3f) % 255) / 100f;//((float)System.Math.Pow(colourCounter, 1.2) % (float)255) / 100;
+                //b = ((colourCounter * 4f) % 255) / 100f;//((float)System.Math.Pow(colourCounter, 1.3) % (float)255) / 100;
+
+                //r = ((float)System.Math.Pow(1.1,colourCounter) % (float)255) / 100;
+                //g = ((float)System.Math.Pow(1.2,colourCounter) % (float)255) / 100;
+                //b = ((float)System.Math.Pow(1.3,colourCounter) % (float)255) / 100;
+
+                //r = ((float)((37 * 2) % 100)) / 100f;
+                //g = ((float)((colourCounter * 3) % 100)) / 100f;
+                //b = ((float)((colourCounter * 2) % 100)) / 100f;
+
+                //Debug.Log("colourCounter: " +colourCounter+"R: "+r+" G: " +g +" B: " +b);
+                
+                string hexString = "#" +hexColour.ToString("X");
+                Debug.Log("Hex:"+ hexString);
+                if(ColorUtility.TryParseHtmlString(hexString, out colour))
+                {
+                    _sampleCube[i].transform.GetChild(0).gameObject.GetComponent<Renderer>().material.SetColor("_Color",colour);
+                    Debug.Log("heerio");
+                }
+
+                //SetColor("_Color",new Color(r,g,b));
                ////_sampleCube[i].transform.Rotate(transform.up * rotateSpeed * Time.deltaTime);
                 //_sampleCube[i].transform.RotateAround(Vector3.zero,Vector3.up,rotateSpeed * Time.deltaTime);
                 // _sampleCube[i].transform.LookAt(_cylinder.transform);
+                Debug.Log(hexColour);
             }
             if(colourUp)
             {
-                colourCounter++;
+                hexColour+=  1;
+                Debug.Log("Up:" + hexColour);
             }
             else
             {
-                colourCounter-=1;
+                hexColour-=1;
+                Debug.Log("Down:" + hexColour);
             }
-            if(i == (sampleSize / 2f))
+            if(hexColour >= 16777215 || hexColour <= 0)
             {
                 colourUp = !(colourUp);
+                Debug.Log("pog");
             }
         }
         //_sampleCubePrefab.transform.Rotate(yAngle:.01f,xAngle:0,zAngle:0,relativeTo:Space.Self);
+        
     }
 }
