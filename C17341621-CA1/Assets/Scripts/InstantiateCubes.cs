305edfcc7795a18e7bffc780a002b0b1af8e7c31
c17341621 <c17341621@mydit.ie>
2020-12-22 20:13:28 +0000

    Changing colours

--- C17341621-CA1/Assets/Scripts/InstantiateCubes.cs
+++ C17341621-CA1/Assets/Scripts/InstantiateCubes.cs
@@ -9,37 +9,76 @@ public class InstantiateCubes : MonoBehaviour
 
     public float rotateSpeed = 10.0f;
     GameObject[] _sampleCube = new GameObject[512];
-    public float _maxScale = 100;
+    public float _maxScale = 1500;
     // Start is called before the first frame update
+
+    int sampleSize = 250;
+    int numPillars = 360;
     void Start()
     {
-        for(int i = 0;i < 512; i++)
+        Vector3 tempVector;
+        for(int i = 0;i < numPillars; i++)
         {
             GameObject _instanceSampleCube = (GameObject)Instantiate (_sampleCubePrefab);
             _instanceSampleCube.transform.position = this.transform.position;
             _instanceSampleCube.transform.parent = this.transform;
             _instanceSampleCube.name = "SampleCube" + i;
-            //this.transform.eulerAngles = new Vector3 (0,-0.703125f * i,0);
-            this.transform.eulerAngles = new Vector3 (0,0 + i,0);
-            _instanceSampleCube.transform.position = Vector3.forward * _cylinder.transform.localScale.x;
+            this.transform.eulerAngles = new Vector3 (0,(-(360/360)) * i,0);
+            //this.transform.eulerAngles = new Vector3 (0,0 + i,0);
+            _instanceSampleCube.transform.position = Vector3.forward * (_cylinder.transform.localScale.x) / 2;
             _sampleCube[i] = _instanceSampleCube;
+            //_sampleCube[i].transform.GetChild(0).AddComponent<Renderer>();
+            
+
+
+            tempVector = _sampleCube[i].transform.localScale;
+            //tempVector.x = (sampleSize/360);
+            _sampleCube[i].transform.localScale = tempVector;
+            _sampleCube[i].transform.LookAt(_cylinder.transform);
+            
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        float r;
+        float g;
+        float b;
         
-        for (int i = 0;i<512;i++)
+        this.transform.RotateAround(Vector3.zero,Vector3.up,rotateSpeed * Time.deltaTime);
+        
+        Vector3 tempVector;
+        bool colourUp = true;
+        int colourCounter = 0;
+        for (int i = 0;i<numPillars;i++)
         {
             if(_sampleCube != null)
             {
-                _sampleCube[i].transform.localScale = new Vector3(10,10,(AudioVisualiser._samples[i] * _maxScale + 2));
-               //_sampleCube[i].transform.Rotate(transform.up * rotateSpeed * Time.deltaTime);
-                _sampleCube[i].transform.RotateAround(Vector3.zero,Vector3.up,rotateSpeed * Time.deltaTime);
-                 _sampleCube[i].transform.LookAt(_cylinder.transform);
+                tempVector = _sampleCube[i].transform.localScale;
+                tempVector.z = (AudioVisualiser._samples[i % sampleSize] * _maxScale + 2);
+               _sampleCube[i].transform.localScale = tempVector;
+                r = ((colourCounter * 2f) % 255) / 100f; //((float)System.Math.Pow(colourCounter, 1.1) % (float)255) / 100;
+                g = ((colourCounter * 3f) % 255) / 100f;//((float)System.Math.Pow(colourCounter, 1.2) % (float)255) / 100;
+                b = ((colourCounter * 4f) % 255) / 100f;//((float)System.Math.Pow(colourCounter, 1.3) % (float)255) / 100;
+               _sampleCube[i].transform.GetChild(0).gameObject.GetComponent<Renderer>().material.SetColor("_Color",new Color(r,g,b)); //SetColor("_Color",new Color(r,g,b));
+               ////_sampleCube[i].transform.Rotate(transform.up * rotateSpeed * Time.deltaTime);
+                //_sampleCube[i].transform.RotateAround(Vector3.zero,Vector3.up,rotateSpeed * Time.deltaTime);
+                // _sampleCube[i].transform.LookAt(_cylinder.transform);
+            }
+            if(colourUp)
+            {
+                colourCounter++;
+            }
+            else
+            {
+                colourCounter-=1;
+            }
+            if(i == (sampleSize / 2f))
+            {
+                colourUp = !(colourUp);
             }
         }
-        _sampleCubePrefab.transform.Rotate(yAngle:.01f,xAngle:0,zAngle:0,relativeTo:Space.Self);
+        //_sampleCubePrefab.transform.Rotate(yAngle:.01f,xAngle:0,zAngle:0,relativeTo:Space.Self);
     }
 }
